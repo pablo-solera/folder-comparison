6a2a01d81db0140dc24263737f2e8eaacd687a94
Pablo.Avila <pablo.avila@solera.com>
2026-01-19 16:13:40 +0100

    app icon deleted

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 # main.py
 from tkinterdnd2 import TkinterDnD
-import tkinter as tk
 import ui
+
 
 def main():
     root = TkinterDnD.Tk()
     root.title("Comparador de Archivos")
     root.geometry("1000x850")
-    root.iconphoto(True,tk.PhotoImage(file="../img/icon.png"))
     widgets = ui.construir_ui(root)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
